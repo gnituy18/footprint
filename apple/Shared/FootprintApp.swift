52681ac8c774ef9f3d04aef147a08e8e95f06cdd
gnituy18 <sourfeethsu@gmail.com>
2021-02-05 18:21:54 +0800

    Add apple

---
+++ apple/Shared/FootprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootprintApp.swift
+//  Shared
+//
+//  Created by Charlie Hsu on 2021/2/5.
+//
+
+import SwiftUI
+
+@main
+struct FootprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
